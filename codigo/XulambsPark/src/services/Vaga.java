5686c463a18afbf2022cfe463c0e3d5d8c8afe8d
Otto Esteves <149393992+OttoEsteves@users.noreply.github.com>
2024-10-04 15:42:00 -0300

    Update Vaga.java

--- codigo/XulambsPark/src/services/Vaga.java
+++ codigo/XulambsPark/src/services/Vaga.java
@@ -1,23 +1,38 @@
-package services;
-
-import java.time.LocalTime;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Vaga {
-	
-	private String codigoDaVaga;
-	private localTime horarioDeEntrada;
-	private localTime horarioDeSaida;
-	private localData dataDeEntrada;
-	private localData dataDeSaida;
-	
-	
-	
-	
-	public boolean verificarVaga() {
-		
-		
-		
-		
-	}
+    //atributos
+    private String codigoDaVaga;
+
+    //construtor da classe pai
+    public Vaga(String codigoDaVaga) {
+        super();
+        this.codigoDaVaga = codigoDaVaga;
+    }
+
+    //get set
+    public String getCodigoDaVaga() {
+        return codigoDaVaga;
+    }
+
+    public void setCodigoDaVaga(String codigoDaVaga) {
+        this.codigoDaVaga = codigoDaVaga;
+    }
+
+    //metodos
+
+    public boolean verificarVaga(Vaga[] vagas, Vaga vaga) {
+        for (Vaga v : vagas) {
+            if (v.getCodigoDaVaga().equals(vaga.getCodigoDaVaga())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public String getCodigoDaVaga(String codigoDaVaga) {
+        return this.codigoDaVaga.equals(codigoDaVaga) ? this.codigoDaVaga : null;
+    }
+
+
 }
