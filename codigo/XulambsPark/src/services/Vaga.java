2bed6c4d8d8b7945af2d5cd2f82a3822abd1014b
Otto Esteves <149393992+OttoEsteves@users.noreply.github.com>
2024-10-04 16:29:09 -0300

    Update Vaga.java

--- codigo/XulambsPark/src/services/Vaga.java
+++ codigo/XulambsPark/src/services/Vaga.java
@@ -33,6 +33,53 @@ public class Vaga {
     public String getCodigoDaVaga(String codigoDaVaga) {
         return this.codigoDaVaga.equals(codigoDaVaga) ? this.codigoDaVaga : null;
     }
+//idoso: 15% pcd :13% vip:20% mais caro
+
+    // Classe VagaVip
+    public class VagaVip extends Vaga {
+
+        // Construtor da classe VagaVip 
+        public VagaVip(String codigoDaVaga) {
+            super(codigoDaVaga);
+        }
+
+
+        public void IncrementarTarifaVIP(Vaga vaga) {
+
+            System.out.println("Tarifa VIP acrescida em 20% para a vaga: " + vaga.getCodigoDaVaga());
+        }
+    }
+
+    // Classe VagaIdoso
+    public class VagaIdoso extends Vaga {
+
+        // Construtor da classe VagaIdoso
+        public VagaIdoso(String codigoDaVaga) {
+            super(codigoDaVaga);
+        }
+
+
+        public void IncrementarDescontoIdoso(Vaga vaga) {
+
+            System.out.println("Desconto de 15% aplicado para a vaga: " + vaga.getCodigoDaVaga());
+        }
+    }
+
+    // Classe VagaPCD
+    public class VagaPCD extends Vaga {
+
+
+        public VagaPCD(String codigoDaVaga) {
+            super(codigoDaVaga);
+        }
+
+
+        public void IncrementarDescontoPCD(Vaga vaga) {
+
+            System.out.println("Desconto de 13% aplicado para a vaga: " + vaga.getCodigoDaVaga());
+        }
+    }
+
 
 
 }
