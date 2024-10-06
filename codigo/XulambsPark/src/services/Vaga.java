3c7a11d757e49dab3b542313aaa5eec7b53d3d40
Otto Esteves <149393992+OttoEsteves@users.noreply.github.com>
2024-10-06 16:42:49 -0300

    Update Vaga.java

--- codigo/XulambsPark/src/services/Vaga.java
+++ codigo/XulambsPark/src/services/Vaga.java
@@ -1,23 +1,59 @@
-package services;
-
-import java.time.LocalTime;
-import java.time.LocalDate;
-
 public class Vaga {
-	
-	private String codigoDaVaga;
-	private localTime horarioDeEntrada;
-	private localTime horarioDeSaida;
-	private localData dataDeEntrada;
-	private localData dataDeSaida;
-	
-	
-	
-	
-	public boolean verificarVaga() {
-		
-		
-		
-		
-	}
+    private String codigoDaVaga;
+
+    public Vaga(String codigoDaVaga) {
+        this.codigoDaVaga = codigoDaVaga;
+    }
+
+    public String getCodigoDaVaga() {
+        return codigoDaVaga;
+    }
+
+    public void setCodigoDaVaga(String codigoDaVaga) {
+        this.codigoDaVaga = codigoDaVaga;
+    }
+
+    //preço sem desconto
+    public double aplicarDescontoOuAcrescimo(double precoBase) {
+        return precoBase;
+    }
+
+
+    public static class VagaVip extends Vaga {
+
+        public VagaVip(String codigoDaVaga) {
+            super(codigoDaVaga);
+        }
+
+        @Override
+        public double aplicarDescontoOuAcrescimo(double precoBase) {
+            return precoBase * 1.20; // Acréscimo de 20% para vagas VIP
+        }
+    }
+
+    // Subclasse VagaIdoso
+    public static class VagaIdoso extends Vaga {
+
+        public VagaIdoso(String codigoDaVaga) {
+            super(codigoDaVaga);
+        }
+
+        @Override
+        public double aplicarDescontoOuAcrescimo(double precoBase) {
+            return precoBase * 0.85; // Desconto de 15% para vagas Idoso
+        }
+    }
+
+    // Subclasse VagaPCD
+    public static class VagaPCD extends Vaga {
+
+        public VagaPCD(String codigoDaVaga) {
+            super(codigoDaVaga);
+        }
+
+        @Override
+        public double aplicarDescontoOuAcrescimo(double precoBase) {
+            return precoBase * 0.87; // Desconto de 13% para vagas PCD
+        }
+    }
 }
