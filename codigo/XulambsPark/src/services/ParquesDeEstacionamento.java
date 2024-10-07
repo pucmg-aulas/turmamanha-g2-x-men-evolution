0b88ddc727b5f7fc26b82410cb40b17b5ba363dd
Bernardo de Resende Marcelino <bresende66@gmail.com>
2024-10-06 22:06:35 -0300

    Update ParquesDeEstacionamento.java

--- codigo/XulambsPark/src/services/ParquesDeEstacionamento.java
+++ codigo/XulambsPark/src/services/ParquesDeEstacionamento.java
@@ -1,169 +1,94 @@
-public class Vaga {
-    private String idVaga;
-
-
-    public Vaga(String idVaga) {
-        this.idVaga = idVaga;
-    }
-
-
-    public String getIdVaga() {
-        return idVaga;
-    }
-
-    public void setIdVaga(String idVaga) {
-        this.idVaga = idVaga;
-    }
-}
-
-
+package services;
+import services.Vaga;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ParquesDeEstacionamento {
-    private String codigoDoParque;
-    private ArrayList<Vaga> vagas;
+	private String codigoDoParque;
+	private ArrayList<Vaga> vagas;
 
 
-    public ParquesDeEstacionamento(String codigoDoParque) {
-        this.codigoDoParque = codigoDoParque;
-        this.vagas = new ArrayList<>();
-    }
+	public ParquesDeEstacionamento(String codigoDoParque) {
+		this.codigoDoParque = codigoDoParque;
+		this.vagas = new ArrayList<>();
+	}
 
 
-    public void criarVaga(Vaga vaga) {
-        vagas.add(vaga);
-    }
+	public  void criarVaga(Vaga vaga) {
+		System.out.println("Digite um código para a vaga: ");
+		Scanner scanner = new Scanner(System.in);
+		String alterarCodigoDaVaga = scanner.nextLine();
+		vaga.setCodigoDaVaga(alterarCodigoDaVaga);
+	}
 
-    public void atualizarVaga(Vaga vaga) {
-        for (int i = 0; i < vagas.size(); i++) {
-            if (vagas.get(i).getIdVaga().equals(vaga.getIdVaga())) {
-                vagas.set(i, vaga);
-                break;
-            }
-        }
-    }
+	public void atualizarVaga(Vaga vaga) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o código da vaga que deseja atualizar: ");
+		String codigoDaVaga = scanner.nextLine();
+		System.out.println("Digite o novo tipo da vaga (vip, idoso, pcd): ");
+		String novoTipo = scanner.nextLine();
+
+		Vaga novaVaga = Vaga.mudarTipo(vaga, novoTipo);
+		for (int i = 0; i < vagas.size(); i++) {
+			if (vagas.get(i).getCodigoDaVaga().equals(codigoDaVaga)) {
+				vagas.set(i, novaVaga);
+				System.out.println("Vaga atualizada com sucesso.");
+				return;
+			}
+		}
+		System.out.println("Vaga não encontrada.");
+	}
 
 
-    public String getCodigoDoParque() {
-        return codigoDoParque;
-    }
+	public String getCodigoDoParque() {
+		return codigoDoParque;
+	}
 
-    public void setCodigoDoParque(String codigoDoParque) {
-        this.codigoDoParque = codigoDoParque;
-    }
+	public void setCodigoDoParque(String codigoDoParque) {
+		this.codigoDoParque = codigoDoParque;
+	}
 
-    public ArrayList<Vaga> getVagas() {
-        return vagas;
-    }
+	public ArrayList<Vaga> getVagas() {
+		return vagas;
+	}
+	public static void main(String[] args) {
+		ParquesDeEstacionamento parque = new ParquesDeEstacionamento("Parque1");
+		boolean continuar = true;
+		while (continuar) {
+			System.out.println("1 - Criar Vaga");
+			System.out.println("2 - Atualizar Vaga");
+			System.out.println("3 - Listar Vagas");
+			System.out.println("4 - Sair");
+			System.out.print("Escolha uma opção: ");
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				int opcao = scanner.nextInt();
+				scanner.nextLine(); // Consume newline
+				switch (opcao) {
+					case 1:
+						Vaga vaga = new Vaga(""); // Create a new Vaga instance
+						parque.criarVaga(vaga);
+						break;
+					case 2:
+						Vaga vagaParaAtualizar = new Vaga(""); // Create a new Vaga instance
+						parque.atualizarVaga(vagaParaAtualizar);
+						break;
+					case 3:
+						for (Vaga v : parque.vagas) {
+							System.out.println("Vaga: " + v.getCodigoDaVaga());
+						}
+						break;
+					case 4:
+						continuar = false;
+						break;
+					default:
+						System.out.println("Opção inválida.");
+						break;
+				}
+			} catch (Exception e) {
+				System.err.println("Erro ao ler opção: " + e.getMessage());
+			}
+		}
+	}
 }
 
-import java.util.ArrayList;
-
-public class SistemaDeEstacionamento {
-    private ArrayList<ParquesDeEstacionamento> parques;
-
-
-    public SistemaDeEstacionamento() {
-        parques = new ArrayList<>();
-    }
-
-
-    public void cadastrarVaga(Veiculo veiculo, Cliente cliente) {
-
-    }
-
-    public void liberarVaga(Veiculo veiculo, Cliente cliente) {
-
-    }
-
-    public void consultarParques(ArrayList<ParquesDeEstacionamento> parques) {
-
-    }
-
-    public void listarVagas(Vaga vaga, ParquesDeEstacionamento parque) {
-
-    }
-
-    public void getVeiculos(Veiculo veiculo) {
-
-    }
-
-    public ArrayList<Vaga> getVagas(Vaga vaga) {
-
-        return null;
-    }
-}
-
-
-// testes Unitario
-
-//teste 1
-
-//Parques de Estacionamento
-import org.junit.Test;
-import static org.junit.Assert.*;
-        import java.util.ArrayList;
-
-public class ParquesDeEstacionamentoTest {
-
-    @Test
-    public void testCriarVaga() {
-        ParquesDeEstacionamento parque = new ParquesDeEstacionamento("P001");
-        Vaga vaga = new Vaga("V001");
-
-
-        parque.criarVaga(vaga);
-
-
-        assertEquals(1, parque.getVagas().size());
-        assertEquals("V001", parque.getVagas().get(0).getIdVaga());
-    }
-}
-
-//teste 2
-
-//cadastrar vaga
-public class SistemaDeEstacionamento {
-    private ArrayList<ParquesDeEstacionamento> parques;
-
-    public SistemaDeEstacionamento() {
-        parques = new ArrayList<>();
-    }
-
-
-    public void cadastrarVaga(ParquesDeEstacionamento parque, Vaga vaga) {
-        parque.criarVaga(vaga);
-        parques.add(parque);
-    }
-
-    public ArrayList<ParquesDeEstacionamento> getParques() {
-        return parques;
-    }
-}
-
-// retornar esse :
-
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class SistemaDeEstacionamentoTest {
-
-    @Test
-    public void testCadastrarVaga() {
-        SistemaDeEstacionamento sistema = new SistemaDeEstacionamento();
-        ParquesDeEstacionamento parque = new ParquesDeEstacionamento("P002");
-        Vaga vaga = new Vaga("V002");
-
-
-        sistema.cadastrarVaga(parque, vaga);
-
-
-        assertEquals(1, sistema.getParques().size());
-
-
-        assertEquals(1, sistema.getParques().get(0).getVagas().size());
-        assertEquals("V002", sistema.getParques().get(0).getVagas().get(0).getIdVaga());
-    }
-}
-
-// para executar o teste coloque na bash "mavem": mvn test
